d3f77f25a96a3ffbe20075819cef017e5e61fd34
Sahil Khosla <sahil@Sahils-MacBook-Pro.local>
2021-01-09 14:58:53 -0500

    removed redundant babel config and fixed test

--- test/index.test.js
+++ test/index.test.js
@@ -1,20 +1,18 @@
 var assert = require('assert') 
-var index = require('../dist/index');
+var baseNpmLib = require('../dist/index');
 var sinon = require('sinon');
-
-var base = index.default;
 
 describe('base npm lib', function() {
   it('should log hello name', function() {
     var spy = sinon.spy(console, 'log');
-    base.sayHello('name');
+    baseNpmLib.sayHello('name');
     assert(spy.calledWith('hello name'));
     spy.restore();
   });
 
   it('should log hello world', function() {
     var spy = sinon.spy(console, 'log');
-    base.greet();
+    baseNpmLib.greet();
     assert(spy.calledWith('hello world!!'));
     spy.restore();
   });
